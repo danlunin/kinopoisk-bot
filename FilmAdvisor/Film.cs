c7ea84badff0e1b7000ba98855a2c2b43797fe0d
stoniah <ablclanmarazm@gmail.com>
2016-05-19 23:39:57 +0600

    ver 1.1.1

---
+++ FilmAdvisor/Film.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmAdvisor {
+    public class Film : IFilm {
+        public string Name { get; set; }
+
+        public string Year { get; set; }
+
+        public string Country { get; set; }
+
+        public string[] Genre { get; set; }
+
+        public string Rate { get; set; }
+
+        public string Producer { get; set; }
+
+        public Film(string name, string year) {
+            Name = name;
+            year = Year;
+        }
+    }
+}
---
+++ FilmAdvisor/Parameters.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmAdvisor {
+    public class Parameters : IParameters {
+        private Dictionary<string, object> parameters { get; }
+
+        public Parameters() {
+            //эти нужны всегда
+            parameters = new Dictionary<string, object> {
+                ["level"] = 7,
+                ["from"] = "forma",
+                ["result"] = "adv",
+                ["m_act[from]"] = "forma",
+                ["m_act[what]"] = "content"
+            };
+        }
+
+        public void SetName(string name) {
+            parameters["m_act[find]"] = name;
+        }
+
+        public void SetYear(int year) {
+            parameters["m_act[year]"] = year;
+        }
+
+        public void SetGenre(int genre) {
+            parameters["m_act[genre][]"] = genre;
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() {
+            return ((IEnumerable<KeyValuePair<string, object>>)parameters).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable<KeyValuePair<string, object>>)parameters).GetEnumerator();
+        }
+    }
+}
