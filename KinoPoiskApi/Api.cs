8ef45dd14138b5dc572808c4472278b58f5f4d08
stoniah <ablclanmarazm@gmail.com>
2016-06-09 18:56:04 +0600

    kinopoiskApi

---
+++ KinoPoiskApi/Api.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoPoiskApi {
+    public class Api {
+        private Parameters searchParameters;
+        private List<RequiredValueType> requiredValues;
+        private Requester requester;
+        private Parser parser;
+
+        public Api() {
+            searchParameters = new Parameters();
+            requiredValues = new List<RequiredValueType>();
+            requester = new Requester();
+            parser = new Parser();
+        }
+
+        public Api AddSearchParameter(SearchParameterType type, string value) {
+            searchParameters.Add(type, value);
+            return this;
+        }
+
+        public Api AddRequiredValue(RequiredValueType valueType) {
+            requiredValues.Add(valueType);
+            return this;
+        }
+
+        public IEnumerable<M> Get<M>(int v) where M : IMovie {
+            var response = requester.Search(searchParameters);
+            var movieDicts = parser.GetMovies(response, requiredValues);
+            foreach (var dict in movieDicts) {
+                yield return DictTo<M>(dict);
+            }
+        }
+
+        public M DictTo<M>(Dictionary<RequiredValueType, string> dict) where M : IMovie {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KinoPoiskApi/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using RestSharp;
+
+namespace KinoPoiskApi
+{
+    public class Parser {
+        public IEnumerable<Dictionary<RequiredValueType, string>> GetMovies(IRestResponse response, IEnumerable<RequiredValueType> requiredValues) {
+            throw new NotImplementedException();
+        }
+    }
+}
