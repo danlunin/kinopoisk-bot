c60c05ff03c493a572851fe7ce938e4584303d7f
stoniah <ablclanmarazm@gmail.com>
2016-06-09 20:27:46 +0600

    a

--- KinoPoiskApi/Parser.cs
+++ KinoPoiskApi/Parser.cs
@@ -9,8 +9,31 @@ using RestSharp;
 namespace KinoPoiskApi
 {
     public class Parser {
+        public readonly Regex filmPageUrl;
+        
+        public Parser() {
+            filmPageUrl = new Regex(@"^/film/\d+/$");
+        }
+
         public IEnumerable<Dictionary<RequiredValueType, string>> GetMovies(IRestResponse response, IEnumerable<RequiredValueType> requiredValues) {
+            if (IsMoviePage(response.ResponseUri))
+                yield return ParseMoviePage(response, requiredValues);
+            else
+                foreach (var dict in ParseSearchResultPage(response, requiredValues))
+                    yield return dict;
             throw new NotImplementedException();
+        }
+
+        private IEnumerable<Dictionary<RequiredValueType, string>> ParseSearchResultPage(IRestResponse response, IEnumerable<RequiredValueType> requiredValues) {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<RequiredValueType, string> ParseMoviePage(IRestResponse response, IEnumerable<RequiredValueType> requiredValues) {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMoviePage(Uri url) {
+            return filmPageUrl.IsMatch(url.AbsolutePath);
         }
     }
 }
